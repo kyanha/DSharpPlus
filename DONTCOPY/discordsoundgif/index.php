b9b4dc233e44a2b0123222f9d842dee06523adc1
root <root@WordPress-on-1404.071116-362>
2016-09-03 17:03:06 -0400

    REMOVED DONTCOPY

--- DONTCOPY/discordsoundgif/index.php
+++
@@ -1,4 +0,0 @@
-
-<video controls="" autoplay="" name="media">
-          <source src="video.mp4" type="video/mp4">
-     </video>
--- DONTCOPY/tools/md5.php
+++
@@ -1,8 +0,0 @@
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<?php
-	if(isset($_GET[h])){
-		print(md5($_GET[h]));
-	}else{
-		print('<form action="" method="get">input: <input type="text" name="h"><br><input type="submit" value="Submit"></form>');
-	}
-?>
